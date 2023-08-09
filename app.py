0616c3fd5d4fb56cd55b4a92d1e1e75371f65796
Violet Vivirand <purpleslovemail@gmail.com>
2023-08-09 08:15:16 +0800

    Edit comments

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ class EICEStack(Stack):
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        # Create a VPC with 2 private isolated subnets
         vpc = ec2.Vpc(
             self, "EICEVPC",
             max_azs=2, 
@@ -31,27 +32,32 @@ class EICEStack(Stack):
             ],
         )
 
+        # Create a Security Group for EICE
         sg_eice = ec2.SecurityGroup(
             self, "EICESecurityGroup",
             vpc=vpc,
             allow_all_outbound=False,
         )
 
+        # Create a Security Group for EC2 Instance
         sg_ec2 = ec2.SecurityGroup(
             self, "EC2SecurityGroup",
             vpc=vpc,
         )
 
+        # Allow SSH from EICE to EC2 Instance
         sg_eice.add_egress_rule(
             peer=sg_ec2,
             connection=ec2.Port.tcp(22),
         )
         
+        # Allow SSH from EC2 Instance to EICE
         sg_ec2.add_ingress_rule(
             peer=sg_eice,
             connection=ec2.Port.tcp(22),
         )
 
+        # Create a EC2 Instance
         bastion = ec2.Instance(
             self, "EICEBastion",
             vpc=vpc,
@@ -61,13 +67,14 @@ class EICEStack(Stack):
             security_group=sg_ec2,
         )
         
+        # Create a Custom Resource for EICE Instance Connect Endpoint
         eice = cr.AwsCustomResource(
             self, "EC2InstanceConnectionEndpoint",
             install_latest_aws_sdk=True,
             on_update=cr.AwsSdkCall(  # Ref: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#createInstanceConnectEndpoint-property
                 service="EC2",
                 action="createInstanceConnectEndpoint",
-                parameters={  # [TODO] Dump to JSON format?
+                parameters={
                     "DryRun": False,
                     "SubnetId": vpc.select_subnets(subnet_type=ec2.SubnetType.PRIVATE_ISOLATED).subnet_ids[0],
                     "SecurityGroupIds": [sg_eice.security_group_id],
@@ -78,7 +85,7 @@ class EICEStack(Stack):
             on_delete=cr.AwsSdkCall(  # Ref: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/EC2.html#deleteInstanceConnectEndpoint-property
                 service="EC2",
                 action="deleteInstanceConnectEndpoint",
-                parameters={  # [TODO] Dump to JSON format?
+                parameters={
                     "DryRun": False,
                     "InstanceConnectEndpointId": cr.PhysicalResourceIdReference(),
                 }
